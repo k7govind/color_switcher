7a61c64a42f4722bc6fa9d84a04bff46bf1ade9b
Govindarajan K <govindra@michaels.com>
2025-01-23 19:01:14 +0530

    updated change with RGB function

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const button = document.getElementById('mybutton');
 
 button.addEventListener('click',()=>{
     const bgcolorDiv = document.getElementById('bgcolor');
-    let colorArr = ['blue','red','pink','lime','hotpink','green','yellow','violet'];
-    let chosenColor = colorArr[Math.floor(Math.random()  * colorArr.length)];
+    //let colorArr = ['blue','red','pink','lime','hotpink','green','yellow','violet'];
+    //let chosenColor = colorArr[Math.floor(Math.random()  * colorArr.length)];
+    let chosenColor = 'RGB('+Math.floor(Math.random() * 255)+','+Math.floor(Math.random() * 255)+','+Math.floor(Math.random() * 255)+')';
     bgcolorDiv.innerText = 'Background Color: '+ chosenColor.toUpperCase();
     bgcolorDiv.style.backgroundColor = 'white';
     bgcolorDiv.style.border = 'black';
