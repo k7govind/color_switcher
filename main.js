205d70867e19e0d1fbb1a6ece8a22c2fd2ab0bd9
Govindarajan K <govindra@michaels.com>
2025-01-23 18:52:43 +0530

    fixed color changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const button = document.getElementById('mybutton');
 
 button.addEventListener('click',()=>{
     const bgcolorDiv = document.getElementById('bgcolor');
-    let colorArr = ['blue','red','pink','lime','hot pink','green','yellow','violet'];
+    let colorArr = ['blue','red','pink','lime','hotpink','green','yellow','violet'];
     let chosenColor = colorArr[Math.floor(Math.random()  * colorArr.length)];
     bgcolorDiv.innerText = 'Background Color: '+ chosenColor.toUpperCase();
     bgcolorDiv.style.backgroundColor = 'white';
